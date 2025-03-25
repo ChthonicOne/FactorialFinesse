c7dfb75a7ce20f84ee5d5eee82a3f58a0d12d881
ChthonicOne <Chthonicone@gmail.com>
2025-03-25 14:20:01 -0400

    Update FactFinesse.java

--- src/net/FactFinesse.java
+++ src/net/FactFinesse.java
@@ -1,8 +1,3 @@
-/*
- * Copyright 2025
- * Mel Pelchat 
- * a.k.a. Chthonic One
- */
 package net;
 
 import java.util.ArrayList;
@@ -17,10 +12,10 @@ public class FactFinesse {
 		factList.add(24l); // 4!  This is the first valid factorial there exists a solution for.
 		long upper = (long) (factList.get(nextFact - 4) * 2);
 		while (true) {
-			long lower = upper - nextFact * 2; // The lower number is always 2*n lower than the upper number where n!
-			long fact = upper - (lower / 2);   // Lower is always even
+			long halfLower = upper/2 - nextFact; // The lower number is always 2*n lower than the upper number where n!
+			long fact = upper - halfLower;
 			if (fact == factList.get(nextFact - 4)) {
-				System.out.println(String.format(Locale.US, "%d - %d * 0.5 = %d!", (long)upper, (long)lower, nextFact));
+				System.out.println(String.format(Locale.US, "%d - %d * 0.5 = %d!", (long)upper, (long)halfLower * 2, nextFact));
 				nextFact++;
 				if ((double) factList.get(nextFact - 5) * nextFact < Long.MAX_VALUE) {
 					factList.add(factList.get(nextFact - 5) * nextFact);
