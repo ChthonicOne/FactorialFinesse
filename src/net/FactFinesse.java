eb4d6bea12501bc59bae26f33f575d5b37a4c6c0
ChthonicOne <Chthonicone@gmail.com>
2025-03-28 08:18:22 -0400

    Update FactFinesse.java
    Optimized math.

--- src/net/FactFinesse.java
+++ src/net/FactFinesse.java
@@ -10,21 +10,17 @@ public class FactFinesse {
 		int nextFact = 4;
 		List<Long> factList = new ArrayList<>();
 		factList.add(24l); // 4!  This is the first valid factorial there exists a solution for.
-		long upper = (long) (factList.get(nextFact - 4) * 2);
+		long upper = (long) (factList.get(nextFact - 4) * 2) - 2 * nextFact;
 		while (true) {
 			long halfLower = upper/2 - nextFact; // The lower number is always 2*n lower than the upper number where n!
-			long fact = upper - halfLower;
-			if (fact == factList.get(nextFact - 4)) {
-				System.out.println(String.format(Locale.US, "%d - %d * 0.5 = %d!", (long)upper, (long)halfLower * 2, nextFact));
-				nextFact++;
-				if ((double) factList.get(nextFact - 5) * nextFact < Long.MAX_VALUE) {
-					factList.add(factList.get(nextFact - 5) * nextFact);
-					upper = (long) (factList.get(nextFact - 4) * 2); // The number we are searching for approaches 2x n! as n-> infinity
-				} else {
-					break;
-				}
+			System.out.println(String.format(Locale.US, "%d - %d * 0.5 = %d!", (long)upper, (long)halfLower * 2, nextFact));
+			nextFact++;
+			if ((double) factList.get(nextFact - 5) * nextFact < Long.MAX_VALUE) {
+				factList.add(factList.get(nextFact - 5) * nextFact);
+				upper = (long) (factList.get(nextFact - 4) * 2) - 2 * nextFact; // The number we are searching for approaches 2x n! as n-> infinity
+			} else {
+				break;
 			}
-			upper -= 2; // Faster to search backwards from 2n than to search forwards.
 		}
 	}
 }
